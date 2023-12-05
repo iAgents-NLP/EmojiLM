04b135dcaafd8aeb80bf4bcdfc90cce13f2ef437
Li, Sheng-Wei <pha123661@gmail.com>
2023-12-05 23:00:57 +0800

    Refactor inference code

--- inference_lora.py
+++ inference_lora.py
@@ -11,6 +11,8 @@ class EmojiLM:
         self.device = 'cuda'
         self.task_prefix = "emoji: "
         self.prepare_model(model_name, lora_path)
+        text_input = "那你很厲害誒"
+        print("開機測試：", text_input, self.serve(text_input))
 
     def prepare_model(self, model_name, lora_path):
         self.tokenizer = AutoTokenizer.from_pretrained(
@@ -23,13 +25,13 @@ class EmojiLM:
         self.model.eval()
         self.model.to(self.device)
 
+    @torch.no_grad()
     def serve(self, inputs):
         inputs = [self.task_prefix + inputs]
         model_inputs = self.tokenizer(
             inputs, max_length=128, padding='do_not_pad', truncation=True, return_tensors="pt")
-        with torch.no_grad():
-            outputs = self.model.generate(
-                input_ids=model_inputs["input_ids"].to(self.device), max_new_tokens=10)
+        outputs = self.model.generate(
+            input_ids=model_inputs["input_ids"].to(self.device), max_new_tokens=5, do_sample=True, top_p=0.9, top_k=0, temperature=0.8)
 
         ret = self.tokenizer.batch_decode(
             outputs.detach().cpu().numpy(), skip_special_tokens=True)[0]
@@ -47,7 +49,10 @@ def main():
     args = parse_args()
     LM = EmojiLM(args.model, args.lora)
     while True:
-        inp = input("Enter input: ")
+        try:
+            inp = input("Enter input: ")
+        except (KeyboardInterrupt, EOFError):
+            break
         print(inp, LM.serve(inp))
 
 
